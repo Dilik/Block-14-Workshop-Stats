361a5344954a5426e2254351f6f9318fac49d7af
Dilik <1989deny@gmail.com>
2023-10-27 23:49:53 +0500

    Done work.Need_chech

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
 /**
@@ -45,7 +45,11 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  let Sum=0;
+  for (i=0; i< numbers.length; i++) {
+    Sum += numbers[i];
+  }
+  return Sum;
 }
 
 /**
@@ -53,7 +57,7 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  // TODO
+  return getSum(numbers)/getLength(numbers); 
 }
 
 /**
@@ -61,7 +65,13 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
-  // TODO
+  let min = numbers[0];
+  for (i=1; i<numbers.length; i++) {
+    if (numbers[i] < min){
+      min = numbers[i];
+    }
+  }
+  return min;
 }
 
 /**
@@ -69,7 +79,13 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
-  // TODO
+  let max = numbers[0];
+  for (i=1; i < numbers.length; i++) {
+    if (numbers[i]>max){
+      max = numbers[i];
+    }
+  }
+  return max;
 }
 
 /**
@@ -77,7 +93,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return `${getMin(numbers)}-${getMax(numbers)}`;
 }
 
 /**
@@ -85,7 +101,12 @@ function getRange(numbers) {
  * @returns {number[]} the even numbers in the array
  */
 function getEvens(numbers) {
-  // TODO
+  const even=[];
+  for (number in numbers){
+    if ( number>0 && number%2 === 0)
+      even.push(number);
+  }
+  return even;
 }
 
 /**
@@ -93,5 +114,10 @@ function getEvens(numbers) {
  * @returns {number[]} the odd numbers in the array
  */
 function getOdds(numbers) {
-  // TODO
+  const odd=[];
+  for (number in numbers) {
+    if (number>0 && number%2 !== 0)
+      odd.push(number);
+  }
+  return odd;
 }
